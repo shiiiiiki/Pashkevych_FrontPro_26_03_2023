e27d4f7887d445f7a9b4070be3f1b442634f09ce
Tanya Pashkevich <shhhiki313@gmail.com>
2023-03-30 14:54:38 +0300

    homework 2

---
+++ script.js
@@ -0,0 +1,16 @@
+"use strict";
+
+// Task 1
+let name = 'John';
+let admin;
+admin = name;
+alert(admin);
+
+// Task 2
+const ourPlanetName = 'Earth';
+let currentUserName = 'Steve';
+
+// Task 3
+let number = '10' + 10;
+console.log(number);
+console.log(typeof number);
