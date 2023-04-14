0124ee00a80a7934eeb5a5f93096640d600645e1
Tanya Pashkevich <shhhiki313@gmail.com>
2023-04-14 21:52:44 +0300

    hw8

--- js/script.js
+++ js/script.js
@@ -1,1 +1,16 @@
 "use strict";
+const num1 = 5;
+const num2 = 8;
+const sum = function (num1, num2) {
+  return num1 + num2;
+};
+const subt = function (num1, num2) {
+  return num1 - num2;
+};
+const mult = function (num1, num2) {
+  return num1 * num2;
+};
+const resultsum = sum(num1, num2);
+const resultsubt = subt(num1, num2);
+const resultmult = mult(num1, num2);
+console.log(resultsum, resultmult, resultsubt);
