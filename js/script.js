6863761a708a30eb073bd63e38779400841c16e3
Tanya Pashkevich <shhhiki313@gmail.com>
2023-04-15 12:02:34 +0300

    hw16

--- js/script.js
+++ js/script.js
@@ -1,1 +1,10 @@
 "use strict";
+const num = prompt("Enter your number");
+const deg = prompt("Enter your degree");
+const result = function (num, deg){
+    if ((isNaN(num)) || (isNaN(deg))){
+        return "Some error";
+    }
+    return Math.pow(num, deg);
+}
+alert(result(num, deg));
