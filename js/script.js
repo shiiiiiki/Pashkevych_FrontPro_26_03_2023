a174a21d7cc0514e2a7d1cfcde083a3f0f79b9a8
Tanya Pashkevich <shhhiki313@gmail.com>
2023-04-17 12:51:49 +0300

    hw17

--- js/script.js
+++ js/script.js
@@ -1,1 +1,23 @@
 "use strict";
+const padString = function (str, numLen, symb = " ", side = false) {
+  if (!str || typeof str !== "string") {
+    return "Some error. The argument must be a String!";
+  }
+  if (!numLen || typeof numLen !== "number") {
+    return "Some error. The argument must be a Number!";
+  }
+  if (!symb || typeof symb !== "string" || symb.length !== 1) {
+    return "Some error. The argument must be a String in one Symbol!";
+  }
+  if (!side || typeof side !== "boolean") {
+    return "Some error. The argument must be a Boolean!";
+  }
+  if (numLen < str.length) {
+    return str.substr(0, numLen);
+  }
+  const padStr = symb.repeat(numLen - str.length);
+  return side ? padStr + str : str + padStr;
+};
+console.log(padString("hello", 8, "*"));
+console.log(padString("hello", 6, "*", true));
+console.log(padString("hello", 2));
