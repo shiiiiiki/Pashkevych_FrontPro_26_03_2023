387c5a214759d96e7482c61f6d7cdd77f8f39443
Tanya Pashkevich <shhhiki313@gmail.com>
2023-04-19 10:54:27 +0300

    hw19

--- js/script.js
+++ js/script.js
@@ -1,1 +1,10 @@
 "use strict";
+const potatoNeeded = function (liters) {
+  const potatoForLiter = 4;
+  const potatoWeight = 75;
+  const potatoesToBuy = liters * potatoForLiter;
+  const weightToBuy = (potatoesToBuy * potatoWeight) / 1000;
+  const priceForKg = 13;
+  return Math.round(weightToBuy * priceForKg);
+};
+console.log(potatoNeeded(48));
