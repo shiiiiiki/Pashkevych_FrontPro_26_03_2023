7efbaaf43dacbd6cf2446a41f5743a3869d5c9df
Tanya Pashkevich <shhhiki313@gmail.com>
2023-04-19 11:15:17 +0300

    hw20

--- js/script.js
+++ js/script.js
@@ -1,1 +1,11 @@
 "use strict";
+const fib = function (n) {
+  const result = [0, 1];
+  for (let i = 2; i <= n; i++) {
+    const n1 = result[i - 1];
+    const n2 = result[i - 2];
+    result.push(n1 + n2);
+  }
+  return result[n];
+};
+console.log(fib(5));
