bfa786e28e6fb344fe80c879c7ad70e93fa51a9d
Tanya Pashkevich <shhhiki313@gmail.com>
2023-04-19 10:14:39 +0300

    hw22

--- js/script.js
+++ js/script.js
@@ -1,1 +1,35 @@
 "use strict";
+
+function mainFunction(callback) {
+  const num1 = Number(prompt("Enter first number"));
+  const num2 = Number(prompt("Enter second number"));
+  callback(num1, num2);
+}
+
+function exponentiation(num1, num2) {
+  const result = Math.pow(num1, num2);
+  alert("Result of exponentiation is " + result);
+}
+
+mainFunction(exponentiation);
+
+function multiplay(num1, num2) {
+  const resultMult = num1 * num2;
+  alert("Result of multiplication is " + resultMult);
+}
+
+mainFunction(multiplay);
+
+function division(num1, num2) {
+  const resultDiv = num1 / num2;
+  alert("Result of division is " + resultDiv);
+}
+
+mainFunction(division);
+
+function modulo(num1, num2) {
+  const resultDiv = num1 % num2;
+  alert("Result of modulo is " + resultDiv);
+}
+
+mainFunction(modulo);
