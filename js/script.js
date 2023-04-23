29fe867cf6ae2c89d41abf98068d9d3e9962796b
Tanya Pashkevich <shhhiki313@gmail.com>
2023-04-23 12:42:50 +0300

    hw14

--- js/script.js
+++ js/script.js
@@ -1,1 +1,14 @@
 "use strict";
+const num = parseInt(prompt("Enter your number"));
+let isSimple = true;
+if (num <= 1) {
+  isSimple = false;
+} else {
+  for (let i = 2; i < num; i++) {
+    if (num % i === 0) {
+      isSimple = false;
+      break;
+    }
+  }
+}
+console.log(isSimple);
