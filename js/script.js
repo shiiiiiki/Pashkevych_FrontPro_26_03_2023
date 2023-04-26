7302b4de053dc09b16847f760f540090848d5a2f
Tanya Pashkevich <shhhiki313@gmail.com>
2023-04-26 12:11:04 +0300

    hw15

--- js/script.js
+++ js/script.js
@@ -1,1 +1,14 @@
 "use strict";
+const num = prompt("Enter your number");
+if (num < 1) {
+  console.log("NaN");
+} else {
+  let i = 2;
+  while (i <= num) {
+    if (num % i === 0) {
+      console.log(i);
+      break;
+    }
+    i++;
+  }
+}
