a6d844557c867d0357934635c364ac8c32310c68
Tanya Pashkevich <shhhiki313@gmail.com>
2023-04-26 12:24:18 +0300

    hw18

--- js/script.js
+++ js/script.js
@@ -1,1 +1,20 @@
 "use strict";
+const arr = [1, 2, 3, -1, -2, -3];
+const positNum = function (arr) {
+  const exampleArr = [];
+  if (arr.length === 0) {
+    console.log("Array is empty");
+  }
+  for (let i = 0; i < arr.length; i++) {
+    const currentPositiveNumber = arr[i];
+    if (currentPositiveNumber > 0) {
+      exampleArr.push(currentPositiveNumber);
+    }
+  }
+  if (exampleArr.length === 0) {
+    return null;
+  }
+  return exampleArr;
+};
+
+console.log(positNum(arr));
