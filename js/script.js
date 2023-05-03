bb04f1ff0ff6387eb0773ccf5842d8f2c58f8c27
Tanya Pashkevich <shhhiki313@gmail.com>
2023-05-03 22:30:54 +0300

    hw27

--- js/script.js
+++ js/script.js
@@ -1,1 +1,12 @@
 "use strict";
+const numPalindrom = function (num, step = 0) {
+  const reverseNum = Number(String(num).split("").reverse().join(""));
+  const sum = num + reverseNum;
+  if (sum === Number(String(sum).split("").reverse().join(""))) {
+    return { result: sum, step };
+  } else {
+    return numPalindrom(sum, step + 1);
+  }
+};
+const result = numPalindrom(96);
+console.log(result);
