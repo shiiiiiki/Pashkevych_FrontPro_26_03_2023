36099e24f1d3f0b57ef82a98d645fbb1b3edeef6
Tanya Pashkevich <shhhiki313@gmail.com>
2023-05-07 13:54:38 +0300

    hw29

--- js/script.js
+++ js/script.js
@@ -1,1 +1,22 @@
 "use strict";
+const calculator = {
+  num1: 0,
+  num2: 0,
+  read: function () {
+    this.num1 = Number(prompt("Enter first number"));
+    this.num2 = Number(prompt("Enter second number"));
+
+    if (isNaN(this.num1) || isNaN(this.num2)) {
+      alert("One or both of the inputs are not numbers");
+    }
+  },
+  sum: function () {
+    return this.num1 + this.num2;
+  },
+  mul: function () {
+    return this.num1 * this.num2;
+  },
+};
+calculator.read();
+alert(calculator.sum());
+alert(calculator.mul());
