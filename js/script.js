d5953a6aa856d988487c8c53ddf6185eeffa56a0
Tanya Pashkevich <shhhiki313@gmail.com>
2023-05-10 10:21:40 +0300

    hw28

--- js/script.js
+++ js/script.js
@@ -1,1 +1,22 @@
 "use strict";
+const arr = [[1, 2], [3, 4], [[5, 6]], [[[4, 0]]]];
+
+const flat = (arr, ...rest) => {
+  if (rest.length > 0) {
+    throw new Error(
+      "Function accepts only 1 argument, too much arguments provided"
+    );
+  }
+
+  const result = [];
+  const flatten = (arr) => {
+    arr.forEach((item) => {
+      Array.isArray(item) ? flatten(item) : result.push(item);
+    });
+  };
+
+  flatten(arr);
+  return result;
+};
+
+console.log(flat(arr));
