834dca5b1ea1d0bdabe178f9972dfefac5269fb8
Tanya Pashkevich <shhhiki313@gmail.com>
2023-05-18 13:05:07 +0300

    hw37

--- js/script.js
+++ js/script.js
@@ -1,1 +1,11 @@
 "use strict";
+const getNum = () => {
+  let result = 0;
+  return function (num) {
+    return (result += num);
+  };
+};
+const sum = getNum();
+console.log(sum(3));
+console.log(sum(5));
+console.log(sum(20));
