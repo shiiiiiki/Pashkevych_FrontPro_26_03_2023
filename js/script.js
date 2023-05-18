89d5f4fa9732737c1f98b86f29d41f039892aa42
Tanya Pashkevich <shhhiki313@gmail.com>
2023-05-18 13:32:19 +0300

    hw38

--- js/script.js
+++ js/script.js
@@ -1,1 +1,18 @@
 "use strict";
+const randomNumber = () => {
+  const arr = [];
+  const number = () => {
+    const num = Math.floor(Math.random() * 100) + 1;
+    if (arr.indexOf(num) !== -1) {
+      return number();
+    } else {
+      arr.push(num);
+      return num;
+    }
+  };
+  return number;
+};
+const getNum = randomNumber();
+console.log(getNum());
+console.log(getNum());
+console.log(getNum());
