a231bf16988a67af81134a07b2e74afa04bca2af
Tanya Pashkevich <shhhiki313@gmail.com>
2023-05-24 15:43:09 +0300

    hw42

--- js/solution.js
+++ js/solution.js
@@ -1,15 +1,31 @@
 "use strict";
-const ulEl = document.getElementById("ulId");
-const liEl = ulEl.getElementsByTagName("li");
+(function () {
+  const liEl = document.querySelectorAll("#ulId li");
 
-for (const li of liEl) {
-  console.log(li.innerText);
-}
+  for (const li of liEl) {
+    console.log(li.textContent);
+  }
+  const ul = document.getElementById("ulId");
+  const ulAtr = Array.from(ul.attributes);
+  const attributeValues = [];
 
-console.log("Общее кол-во элементов:", liEl.length);
+  for (const attribute of ulAtr) {
+    attributeValues.push(attribute.value);
+  }
+  console.log(attributeValues);
 
-const textArr = [];
-for (const li of liEl) {
-  textArr.push(li.innerText);
-}
-console.log(textArr);
+  const attributeNames = ulAtr.map((attribute) => attribute.name);
+  console.log(attributeNames);
+
+  const yourName = "Таня<3";
+  const lastLi = liEl[liEl.length - 1];
+  lastLi.textContent = `Привет меня зовут ${yourName}`;
+  console.log(lastLi);
+
+  const firstLi = liEl[0];
+  firstLi.setAttribute("data-my-name", yourName);
+  console.log(firstLi);
+
+  ul.removeAttribute("data-dog-tail");
+  console.log(ul);
+})();
