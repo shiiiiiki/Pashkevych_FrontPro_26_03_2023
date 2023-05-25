e0c75d424156ebf8a5964de0f50bade0cdc1d0bb
Tanya Pashkevich <shhhiki313@gmail.com>
2023-05-25 10:31:29 +0300

    hw43

--- js/script.js
+++ js/script.js
@@ -1,1 +1,12 @@
 "use strict";
+const array = [1, 2, [1.1, 1.2, [2.1, 2.2, 2.3], 1.3], 3];
+
+const generateList = (array) => {
+  return `<ul>${array.reduce((str, el) => {
+    str += `<li>${Array.isArray(el) ? generateList(el) : el}</li>`;
+    return str;
+  }, "")}</ul>`;
+};
+
+const generatedList = generateList(array);
+document.body.innerHTML = generatedList;
