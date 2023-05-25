df8d430fa64fb18da39b8f3be85a21dc33e5c913
Tanya Pashkevich <shhhiki313@gmail.com>
2023-05-25 17:41:25 +0300

    hw44

--- js/script.js
+++ js/script.js
@@ -1,1 +1,23 @@
 "use strict";
+(function () {
+  function createTable() {
+    const table = document.createElement("table");
+
+    let num = 1;
+    for (let i = 0; i < 10; i++) {
+      const row = document.createElement("tr");
+
+      for (let j = 0; j < 10; j++) {
+        const cell = document.createElement("td");
+        cell.textContent = num;
+        row.appendChild(cell);
+        num++;
+      }
+      table.appendChild(row);
+    }
+    return table;
+  }
+
+  const createdTable = createTable();
+  document.body.append(createdTable);
+})();
