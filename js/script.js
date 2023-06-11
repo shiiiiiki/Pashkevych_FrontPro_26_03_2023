e92c9277b346f64b9d579aadf2516c5cf0889a73
Tanya Pashkevich <shhhiki313@gmail.com>
2023-06-11 21:23:55 +0300

    hw53

--- js/script.js
+++ js/script.js
@@ -1,1 +1,33 @@
 "use strict";
+const deepFreeze = (obj) => {
+  Object.keys(obj).forEach((prop) => {
+    if (typeof obj[prop] === "object" && !Object.isFrozen(obj[prop])) {
+      deepFreeze(obj[prop]);
+    }
+  });
+  return Object.freeze(obj);
+};
+
+let user = {
+  data: {
+    a: 1,
+    b: 2,
+    c: 3,
+    d: {
+      a1: 1,
+      b1: 2,
+      c1: 3,
+      d1: {
+        a2: 3,
+        b2: 3,
+        c2: 3,
+      },
+    },
+  },
+};
+
+deepFreeze(user);
+
+console.log(Object.isFrozen(user));
+console.log(Object.isFrozen(user.a));
+console.log(Object.isFrozen(user.data.b2));
