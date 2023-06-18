5a4afa918d7c4002620deb5e5e88032f873906bb
Tanya Pashkevich <shhhiki313@gmail.com>
2023-06-18 16:30:04 +0300

    hw56

--- js/script.js
+++ js/script.js
@@ -1,1 +1,39 @@
 "use strict";
+// 1
+(() => {
+  function* setIterator(array) {
+    for (let i = 0; i < array.length; i++) {
+      yield i;
+    }
+  }
+
+  const arr = [1, 2, 3];
+  const iterator1 = setIterator(arr);
+  console.log(iterator1.next());
+  console.log(iterator1.next());
+  console.log(iterator1.next());
+  console.log(iterator1.next());
+})();
+
+// 2
+const iter = function () {
+  let lengthItem = this.length;
+  let firstItem = null;
+  let resultObj = {};
+  if (lengthItem > 0) {
+    firstItem = this.shift();
+    resultObj.value = firstItem;
+    resultObj.done = false;
+  } else {
+    resultObj.value = undefined;
+    resultObj.done = true;
+  }
+  console.log(resultObj);
+};
+
+const arr = [1, 2, 3];
+Array.prototype.iterator2 = iter;
+arr.iterator2();
+arr.iterator2();
+arr.iterator2();
+arr.iterator2();
