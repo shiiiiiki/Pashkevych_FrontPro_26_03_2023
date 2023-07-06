3f36f9669002445534926cb81aa546b38756dbce
shhhi <shhhiki313@gmail.com>
2023-07-06 16:10:39 +0300

    hw69

--- js/script.js
+++ js/script.js
@@ -1,1 +1,11 @@
 "use strict";
+(function () {
+  const levelSet = new WeakSet();
+  const addPlayerToLevelSet = (player) => {
+    levelSet.add(player);
+  };
+
+  const player1 = { id: 1, name: "Иван", level: 10 };
+  addPlayerToLevelSet(player1);
+  console.log(levelSet.has(player1));
+})();
