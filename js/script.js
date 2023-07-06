eb012d7b28751a89d65b70ffec7e3bf687df2070
shhhi <shhhiki313@gmail.com>
2023-07-06 16:02:18 +0300

    hw68

--- js/script.js
+++ js/script.js
@@ -1,1 +1,13 @@
 "use strict";
+(function () {
+  const saveGrades = (student, marks) => {
+    const marksMap = new WeakMap();
+    marksMap.set(student, marks);
+    console.log(marksMap.get(student));
+  };
+
+  const student = { name: "Иван Иванов" };
+  const marks = { math: 90, fiz: 85, eng: 95 };
+
+  saveGrades(student, marks);
+})();
