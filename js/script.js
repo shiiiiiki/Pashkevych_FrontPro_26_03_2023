247b3a6b9b34dbef12bbcf491000f959e9802f7c
shhhi <shhhiki313@gmail.com>
2023-07-06 15:42:18 +0300

    hw66

--- js/script.js
+++ js/script.js
@@ -1,1 +1,22 @@
 "use strict";
+(function () {
+  const sum = (goods) => {
+    const priceMap = new Map();
+    goods.forEach(([item, price]) => {
+      priceMap.set(item, price);
+    });
+
+    const totalPrice = Array.from(priceMap.values()).reduce(
+      (acc, price) => acc + price,
+      0
+    );
+    return totalPrice;
+  };
+
+  const goods = [
+    ["футболка", 15],
+    ["шорты", 25],
+    ["носки", 5],
+  ];
+  console.log(sum(goods));
+})();
