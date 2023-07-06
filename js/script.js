efb368bad8ea7fbff9b82e9de0d5c75949c5bbe4
shhhi <shhhiki313@gmail.com>
2023-07-06 15:52:21 +0300

    hw67

--- js/script.js
+++ js/script.js
@@ -1,1 +1,13 @@
 "use strict";
+(function () {
+  const uniqSymbolsCount = (string) => {
+    const uniqueSymbols = new Set();
+    for (const element of string) {
+      uniqueSymbols.add(element);
+    }
+    return uniqueSymbols.size;
+  };
+
+  const newString = "abracadabra";
+  console.log(uniqSymbolsCount(newString));
+})();
