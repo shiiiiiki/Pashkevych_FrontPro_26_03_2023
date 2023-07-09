5ca8228087c06c12d7fcf9634805e88ad7fdcf41
shhhi <shhhiki313@gmail.com>
2023-07-09 12:39:22 +0300

    hw70

--- js/script.js
+++ js/script.js
@@ -1,1 +1,17 @@
 "use strict";
+(function () {
+  const printGreetings = (students) => {
+    students.forEach(({ name, subject }) => {
+      console.log(`Привет, ${name}! Ты изучаешь предмет ${subject}`);
+    });
+  };
+
+  const students = [
+    { name: "John", age: 20, subject: "Math" },
+    { name: "Alice", age: 22, subject: "English" },
+    { name: "Bob", age: 21, subject: "History" },
+    { name: "Angela", age: 30, subject: "Literature" },
+  ];
+
+  printGreetings(students);
+})();
