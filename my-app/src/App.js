38d4724dd577a2472498bd4fbb78b079551a6eba
shhhi <shhhiki313@gmail.com>
2023-08-16 01:08:10 +0300

    hw76

---
+++ my-app/src/App.js
@@ -0,0 +1,15 @@
+import logo from "./logo.svg";
+import "./App.css";
+import Card from "./components/Card";
+
+function App() {
+  return (
+    <div className="App">
+      <Card title="title" />
+      <Card text="text text text" />
+      <Card title="title" text="+ text" />
+    </div>
+  );
+}
+
+export default App;
